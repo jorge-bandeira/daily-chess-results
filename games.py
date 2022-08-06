e3c70fce0f6c24b9c99fbee66d5dc3731c48bca0
IP93 <invictaproject1893@gmail.com>
2022-08-06 18:22:39 +0100

    coded request url with header to return ndjson

--- games.py
+++ games.py
@@ -1,1 +1,19 @@
-import request
+import requests, ndjson
+
+api_url = "https://lichess.org/api/games/user/"
+
+headers = {
+	"Accept": "application/x-ndjson"
+}
+
+params = {
+	"max": "50",
+	"rated": "true",
+	"perfType": "rapid",
+	"pgnInJson": "true"
+}
+
+games_request = requests.get(api_url + "Zee_Haddock", headers = headers, params = params).text
+gdata = ndjson.loads(games_request)
+
+print(gdata)
