23a4367d1d26a8fb535ab897120bd81078fae655
IP93 <invictaproject1893@gmail.com>
2022-08-22 16:24:37 +0100

    created results page

--- main/routes.py
+++ main/routes.py
@@ -6,3 +6,8 @@ from flask import render_template
 def home():
 	print("home")
 	return render_template('home.html')
+
+@app.route('/results')
+def results():
+	print("results")
+	return render_template('results.html')
