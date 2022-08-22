b1c8b7555b9641687010235ac07cb004c85de5fc
IP93 <invictaproject1893@gmail.com>
2022-08-22 17:13:45 +0100

    added form

--- main/__init__.py
+++ main/__init__.py
@@ -3,6 +3,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+secret_key = os.urandom(32)
+app.config['SECRET_KEY'] = secret_key
+
 from main import routes
 
 if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
--- main/routes.py
+++ main/routes.py
@@ -1,11 +1,17 @@
 from main import app
+from main.forms import MainForm
 from flask import render_template
 
 @app.route('/')
-@app.route('/home')
+@app.route('/home', methods=['GET', 'POST'])
 def home():
+	user = None
+	form = MainForm()
+	if form.validate_on_submit():
+		user = form.user.data
+		form.user.data = ''
 	print("home")
-	return render_template('home.html')
+	return render_template('home.html', user = user, form = form)
 
 @app.route('/results')
 def results():
