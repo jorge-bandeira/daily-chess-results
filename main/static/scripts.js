cfc6f0175c6adc16348dea0b4a3109ec038aa26a
IP93 <invictaproject1893@gmail.com>
2022-08-27 00:58:00 +0100

    added loading spinner

--- main/static/scripts.js
+++ main/static/scripts.js
@@ -3,3 +3,10 @@ function displayNumber(x){
 	let numLabel = document.getElementById("gamesNum");
 	numLabel.innerHTML = x;
 }
+
+function displayLoader(){
+	let loadSpinner = document.getElementById("loadSpinner");
+	let mainBox = document.getElementsByClassName("main-box")[0];
+	mainBox.style.display = "none"
+	loadSpinner.style.display = "flex";
+}
