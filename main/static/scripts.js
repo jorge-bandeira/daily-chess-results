aa2c8bff030346c2ab6013e4644f5644eb5962f9
IP93 <invictaproject1893@gmail.com>
2022-08-27 12:44:45 +0100

    switched loader script to scripts.js

--- main/static/scripts.js
+++ main/static/scripts.js
@@ -1,12 +1,22 @@
+// Hide spinner when returning to homepage
+window.onpageshow = function(e) {
+	let loadSpinner = document.getElementById("loadSpinner");
+	let mainBox = document.getElementsByClassName("main-box")[0];
+	mainBox.style.display = "block"
+	loadSpinner.style.display = "none";
+}
+
 // Display number of games selected with the slider
 function displayNumber(x){
 	let numLabel = document.getElementById("gamesNum");
 	numLabel.innerHTML = x;
 }
 
+// Display loading icon when waiting for results
 function displayLoader(){
 	let loadSpinner = document.getElementById("loadSpinner");
 	let mainBox = document.getElementsByClassName("main-box")[0];
 	mainBox.style.display = "none"
 	loadSpinner.style.display = "flex";
 }
+
