95a2e63270c5b89bf119d64d771a332e1a66b329
IP93 <invictaproject1893@gmail.com>
2022-09-12 21:33:46 +0100

    removed local settings from form

--- main/forms.py
+++ main/forms.py
@@ -1,11 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerRangeField, BooleanField
 from wtforms.validators import DataRequired
-from main import local_settings
 
 class MainForm(FlaskForm):
 	user = StringField("...", validators = [DataRequired()])
-	max_games = IntegerRangeField("", default = local_settings.max_games / 2)
+	max_games = IntegerRangeField("", default = 1000)
 	classical = BooleanField()
 	rapid = BooleanField()
 	blitz = BooleanField()
