ebe52e87542ad7c578ea53c404dc8b217695d5b0
IP93 <invictaproject1893@gmail.com>
2023-01-08 18:53:59 +0000

    setup chess.com api connection

--- main/othergames.py
+++ main/othergames.py
@@ -1,13 +1,14 @@
-import requests
+import requests, json
 import dateutil.relativedelta
 from datetime import date
 
 #request structure
 chess_api_url = "https://api.chess.com/pub/player/"
 
-def defineMonths():
+#get a string for every month to consider
+def defineMonths(n):
 	months = []
-	months_to_consider = 3
+	months_to_consider = n - 1
 	today = date.today()
 	current_month = today.strftime("%Y/%m")
 	months.append(current_month)
@@ -16,8 +17,28 @@ def defineMonths():
 		m = today - dateutil.relativedelta.relativedelta(months = i)
 		months.append(m.strftime("%Y/%m"))
 		i += 1
-	print(months)
 	return months
 
-def getData(user, max, time_control):
-	url = chess_api_url + user
+#make API request
+def createRequest(url):
+	request = requests.get(url)
+	if request.status_code == 404:
+		print("404 error")
+		return 'error'
+	else:
+		response = request.text
+		return response
+
+def getData(user, n_months):
+	months = defineMonths(n_months)
+	responses = []
+	for m in months:
+		url = chess_api_url + user + "/games/" + m[0:4] + "/" + m[5:7]
+		response_data = createRequest(url)
+		if response_data != "error":
+			responses.append(response_data)
+	
+		
+	
+
+
